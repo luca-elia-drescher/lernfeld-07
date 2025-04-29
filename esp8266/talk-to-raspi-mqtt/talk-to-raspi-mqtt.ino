4ac56272882438cc22b9b764074399acc6bddce2
Luca Elia Drescher <148905434+luca-elia-drescher@users.noreply.github.com>
2025-04-29 09:13:45 +0200

    added talk-to-raspi-mqtt.ino

---
+++ esp8266/talk-to-raspi-mqtt/talk-to-raspi-mqtt.ino
@@ -0,0 +1,57 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+ 
+const char* SSID = "IT-Berufe";
+const char* PSK = "79235686";
+const char* MQTT_BROKER = "192.168.1.104";
+int counter = 0;
+ 
+WiFiClient espClient;
+PubSubClient client(espClient);
+ 
+void setup() {
+    Serial.begin(115200);
+    setup_wifi();
+    client.setServer(MQTT_BROKER, 1883);
+    client.setCallback(callback);
+}
+ 
+void setup_wifi() {
+    WiFi.begin(SSID, PSK);
+ 
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(100);
+    }
+ 
+    Serial.println(WiFi.localIP());
+}
+void callback(char* topic, byte* payload, unsigned int length) {
+    String msg;
+    for (byte i = 0; i < length; i++) {
+        char tmp = char(payload[i]);
+        msg += tmp;
+    }
+    Serial.println(msg);
+}
+ 
+void loop() {
+    if (!client.connected()) {
+        while (!client.connected()) {
+            client.connect("ESP8266Client");
+            client.subscribe("pel/test");
+            Serial.println("broker connected");
+            delay(100);
+        }
+    }
+    client.loop();
+ 
+    counter++;
+ 
+    char msg[20];
+ 
+    sprintf(msg, "Luca: %d", counter);
+ 
+    client.publish("pel/test", msg);
+ 
+    delay(2000);
+}
